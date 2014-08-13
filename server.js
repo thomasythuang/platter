0d59a62d0799ecb8328d4b5150a1500232f99ab7
thomasythuang <thomas.yt.huang@gmail.com>
2014-08-13 09:54:33 -0400

    clean server.js

--- server.js
+++ server.js
@@ -1,41 +1,37 @@
-// server.js (final)
+// server.js
 
 // set up ======================================================================
-var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port  	 = process.env.PORT || 8080; 				// set the port
-var passport = require('passport');
-var flash 	 = require('connect-flash');
+var express  	= require('express');
+var app      	= express(); 					// create app w/ express
+var mongoose 	= require('mongoose'); 			// mongoose for mongodb
+var port  		= process.env.PORT || 8080; 	// set port
+var passport 	= require('passport');
+var flash 	 	= require('connect-flash');
 
-var morgan 	 		= require('morgan');
-var cookieParser 	= require('cookie-parser');
-var bodyParser 		= require('body-parser');
-var methodOverride 	= require('method-override');
-var session 		= require('express-session');
+// express modules
+var morgan 	 			= require('morgan');
+var cookieParser 		= require('cookie-parser');
+var bodyParser 			= require('body-parser');
+var methodOverride 		= require('method-override');
+var session 			= require('express-session');
 
-var configDB = require('./config/database'); 	// load the database config
 
 // configuration ===============================================================
+var configDB = require('./config/database'); 	// load the database config
 mongoose.connect(configDB.url); 				// connect to mongoDB database on modulus.io
-
 require('./config/passport')(passport);
 
 // express config
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(methodOverride());
-
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 //app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public/views');
-
 // required for passport
 app.use(session({ 	secret: 'bigtombigtombigtom',
 					saveUninitialized: true,
