e4a5b38a1f375527d4b142830a1d2257ecf7f9e1
thomasythuang <thomas.yt.huang@gmail.com>
2014-08-13 09:48:18 -0400

    edit directive for performance issues

--- public/js/directives/coreDirective.js
+++ public/js/directives/coreDirective.js
@@ -5,13 +5,37 @@ var app = angular.module('coreDirective', []);
 app.directive('mainResources', function(){
 	return{
 		restrict: 'E',
-		templateUrl: '/templates/resources.html',
+		// using templateUrl slows down css loading for some reason
+		//templateUrl: '/templates/resources.html',
+		template: 
+		'<link rel="shortcut icon" href="/img/favicon.ico">'+
+		'<link rel="stylesheet" href="/resources/bootstrap.min.css">'+
+		'<link rel="stylesheet" href="/resources/font-awesome.min.css">'+
+		'<link rel="stylesheet" href="/css/main.css">'+
+		'<script src="/resources/jquery-2.1.1.min.js"></script>'+
+		'<script src="/resources/bootstrap.min.js"></script>',
 	};
 });
 
 app.directive('mainNavbar', function(){
 	return{
 		restrict: 'E', 
-		templateUrl: '/templates/navbar.html',
+		//templateUrl: '/templates/navbar.html',
+		template:
+		'<header>'+
+		'<nav class="navbar navbar-default">'+
+			'<div class="container">'+
+		      	'<div class="navbar-header">'+
+		        	'<a class="navbar-brand" href="#">Platter</a>'+
+		      	'</div>'+
+		      	'<ul class="nav navbar-nav navbar-right">'+
+		        	'<li><a href="/"><span class="fa fa-home"></span> Home</a></li>'+
+		        	'<li><a href="/upload"><span class="fa fa-upload"></span> Upload</a></li>'+
+		        	'<li ng-hide="user"><a href="/login"><span class="fa fa-sign-in"></span> Login</a></li>'+
+		        	'<li ng-show="user"><a href="/profile"><span class="fa fa-facebook-square"></span> {{user.facebook.name}}</a></li>'+
+		  		'</ul>'+
+			'</div>'+
+		'</nav>'+
+		'</header>'
 	};
 });
