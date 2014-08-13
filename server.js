b08577fbe41bd27799b70db778bf836e5a8cbca1
thomasythuang <thomas.yt.huang@gmail.com>
2014-08-13 09:59:42 -0400

    another...sublime problem??

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 
 // set up ======================================================================
 var express 		= require('express');
-var app 			= express(); 					// create app w/ express
-var mongoose 		= require('mongoose'); 			// mongoose for mongodb
-var port 			= process.env.PORT || 8080; 	// set port
+var app 		= express(); 			// create app w/ express
+var mongoose 		= require('mongoose'); 		// mongoose for mongodb
+var port 		= process.env.PORT || 8080; 	// set port
 var passport 		= require('passport');
-var flash 			= require('connect-flash');
+var flash 		= require('connect-flash');
 
 // express modules
-var morgan 			= require('morgan');
+var morgan 		= require('morgan');
 var cookieParser 	= require('cookie-parser');
 var bodyParser 		= require('body-parser');
 var methodOverride 	= require('method-override');
@@ -18,7 +18,7 @@ var session 		= require('express-session');
 
 // configuration ===============================================================
 var configDB = require('./config/database'); 	// load the database config
-mongoose.connect(configDB.url); 				// connect to mongoDB database on modulus.io
+mongoose.connect(configDB.url); 		// connect to mongoDB database on modulus.io
 require('./config/passport')(passport);
 
 // express config
@@ -35,10 +35,10 @@ app.set('views', __dirname + '/public/views');
 // required for passport
 app.use(session({ 	secret: 'bigtombigtombigtom',
 					saveUninitialized: true,
-					resave: true})); 		// session config
+					resave: true})); 	// session config
 app.use(passport.initialize());
-app.use(passport.session()); 				// persistent login sessions
-app.use(flash()); 							// use connect-flash for flash messages stored in session
+app.use(passport.session()); 					// persistent login sessions
+app.use(flash()); 						// use connect-flash for flash messages stored in session
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport);
