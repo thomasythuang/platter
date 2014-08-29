2bda1a5f331f0183028c0d5881439facdc89f659
thomasythuang <thomas.yt.huang@gmail.com>
2014-08-28 21:30:26 -0400

    navbar starts to look halfway decent
    edited truncate filter to be able to toggle ellipsis on word breaks

--- public/js/filters/truncate.js
+++ public/js/filters/truncate.js
@@ -23,13 +23,17 @@ angular.module('truncate', [])
         };
     })
     .filter('words', function () {
-        return function (input, words) {
+        return function (input, words, ellipsis) {
             if (isNaN(words)) return input;
             if (words <= 0) return '';
             if (input) {
                 var inputWords = input.split(/\s+/);
                 if (inputWords.length > words) {
-                    input = inputWords.slice(0, words).join(' ') + '...';
+                    if (ellipsis){
+                        input = inputWords.slice(0, words).join(' ') + '...';
+                    }else{
+                        input = inputWords.slice(0, words).join(' ');
+                    }
                 }
             }
             return input;
